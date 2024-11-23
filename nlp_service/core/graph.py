578e9eced51108ccef3ea77064b875f9e2940386
Jason Jiménez Cruz <jasonjimenezcruz@gmail.com>
2024-11-23 22:52:06 +0100

    Fixed graph

--- nlp_service/core/graph.py
+++ nlp_service/core/graph.py
@@ -11,7 +11,7 @@ def add_user_word_token(user_id: str, word: str, lemma: str, text: str) -> None:
         f'MERGE (u:BackendUser {{ user_id: "{user_id}" }}) RETURN u',
         f'MERGE (w:Word {{ word:"{word}", lemma:"{lemma}" }}) RETURN w',
         f'MERGE (t:TextNode {{ text: "{text}"}}) RETURN t',
-        f'MERGE (w)-[:TOKEN_CREATED_BY {{ created_at: datetime("{now}") }}]->(u) WHERE u.user_id = "{user_id}" AND w.word = "{word}" AND w.lemma = "{lemma}" RETURN u'
+        f'MERGE (w)-[:TOKEN_CREATED_BY {{ created_at: datetime("{now}") }}]->(u) WHERE u.user_id = "{user_id}" AND w.word = "{word}" AND w.lemma = "{lemma}" RETURN u',
         f'MERGE (w)-[:BELONGS_TO]->(y) WHERE u.user_id = "{user_id}" AND t.text = "{text}" RETURN w'
     ]
     with handler.driver.session() as session:
