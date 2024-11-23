b75a88e4493675a7967c3598a0400c6d318bb2e2
Jason Jiménez Cruz <jasonjimenezcruz@gmail.com>
2024-11-23 23:03:40 +0100

    Reversed queries

--- nlp_service/core/graph.py
+++ nlp_service/core/graph.py
@@ -11,8 +11,8 @@ def add_user_word_token(user_id: str, word: str, lemma: str, text: str) -> None:
         f'MERGE (u:BackendUser {{ user_id: "{user_id}" }}) RETURN u',
         f'MERGE (w:Word {{ word:"{word}", lemma:"{lemma}" }}) RETURN w',
         f'MERGE (t:TextNode {{ text: "{text}"}}) RETURN t',
-        f'MERGE (w)-[:TOKEN_CREATED_BY {{ created_at: datetime("{now}") }}]->(u) WHERE u.user_id = "{user_id}" AND w.word = "{word}" AND w.lemma = "{lemma}" RETURN u',
-        f'MERGE (w)-[:BELONGS_TO]->(y) WHERE u.user_id = "{user_id}" AND t.text = "{text}" RETURN w'
+        f'MATCH (u:BackendUser), (w:Word) WHERE u.user_id = "{user_id}" AND w.word = "{word}" AND w.lemma = "{lemma}" WITH u,w MERGE (w)-[:TOKEN_CREATED_BY {{ created_at: datetime("{now}") }}]->(u)  RETURN u',
+        f'MATCH (w:BackendUser), (t:TextNode) WHERE  w.word = "{word}" AND w.lemma = "{lemma}" AND t.text = "{text}"MERGE (w)-[:BELONGS_TO]->(t)  RETURN w'
     ]
     with handler.driver.session() as session:
         for query in queries:
